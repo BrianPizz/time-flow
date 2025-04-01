43eff63cf7f9e0f92fa8ee456a6a2d3256807251
BrianPizz <bpizzimentijr@outlook.com>
2025-04-01 19:06:30 -0400

    basic header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/signup">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

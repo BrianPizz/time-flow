0567b48f0a575cb46103f2beaaa4fd5e230b3e91
BrianPizz <bpizzimentijr@outlook.com>
2025-04-02 19:08:15 -0400

    created individual components for sign up/login card/container

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,62 +2,11 @@ import { useState } from "react";
 import {
   TextField,
   Button,
-  Container,
   Typography,
-  Box,
   Link,
-  styled,
 } from "@mui/material";
-
-import Stack from "@mui/material/Stack";
-import MuiCard from "@mui/material/Card";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignSelf: "center",
-  width: "100%",
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: "auto",
-  [theme.breakpoints.up("sm")]: {
-    maxWidth: "450px",
-  },
-  boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
-  ...theme.applyStyles("dark", {
-    boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
-  }),
-}));
-
-const SignInContainer = styled(Stack)(({ theme }) => ({
-  height: "100vh",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  position: "relative",
-
-  minHeight: "100%",
-  padding: theme.spacing(2),
-  [theme.breakpoints.up("sm")]: {
-    padding: theme.spacing(4),
-  },
-  "&::before": {
-    content: '""',
-    display: "block",
-    position: "absolute",
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-    backgroundRepeat: "no-repeat",
-    ...theme.applyStyles("dark", {
-      backgroundImage:
-        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-    }),
-  },
-}));
+import AuthContainer from "@/components/auth/AuthContainer";
+import AuthCard from "@/components/auth/AuthCard";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -90,8 +39,8 @@ export default function LoginPage() {
   };
 
   return (
-    <SignInContainer>
-      <Card>
+    <AuthContainer>
+      <AuthCard>
         <Typography variant="h4" gutterBottom>
           Login
         </Typography>
@@ -137,7 +86,7 @@ export default function LoginPage() {
             </Link>
           </Typography>
         </form>
-      </Card>
-    </SignInContainer>
+      </AuthCard>
+    </AuthContainer>
   );
 }
